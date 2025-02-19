43e74086be58d9effea6f455ecd3dca89a8bf66d
NikhilPatodia <npatodia085@gmail.com>
2025-02-19 13:25:35 +0530

    add comments

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function UserProfile({params}) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function UserProfile({params}:any) {
     const {id} = params;
   return (
     <div>UserProfile: {id}</div>
